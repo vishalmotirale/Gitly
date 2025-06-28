8c9de6940e2aae8fd8ead0156ecb9460a1ceeb08
vishalmotirale <vishalmotirale111@gmail.com>
2025-06-28 16:27:41 +0530

    Fix. bind to 0.0.0.0 for Rander

--- app.py
+++ app.py
@@ -89,4 +89,5 @@ def logout():
 def not_found(e): return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 1000))
+    app.run(host="0.0.0.0", port=port)
