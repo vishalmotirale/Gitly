70f84bc944d2be0fe915be0a64e24b724cd12406
vishalmotirale <vishalmotirale111@gmail.com>
2025-07-13 20:53:45 +0530

    Dobugging

--- tracking/__init__.py
+++
@@ -1,4 +0,0 @@
-# tracking/__init__.py
-
-# This makes 'tracking' a proper Python package
-# Nothing needs to go here unless you want to pre-load something
--- tracking/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_activity (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    path TEXT,
-    params TEXT,
-    user_agent TEXT,
-    ip_address TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-);
